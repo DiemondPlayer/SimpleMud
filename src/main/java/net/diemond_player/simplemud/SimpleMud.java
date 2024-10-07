f99fb1bc5e189b49c814f2596c42c3d055515cc8
Diemond_Player <diemondplay@gmail.com>
2024-10-07 18:14:59 +0500

    Initial commit

---
+++ src/main/java/net/diemond_player/simplemud/SimpleMud.java
@@ -0,0 +1,15 @@
+package net.diemond_player.simplemud;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleMud implements ModInitializer {
+	public static final String MOD_ID = "simplemud";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
